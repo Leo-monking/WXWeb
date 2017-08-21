3dd533147735d86bec2401096bf470bf049c3e7e
Leo-monking <m_hmj@126.com>
2017-08-21 13:38:32 +0800

    包引用错误
    menu、basic，引用包中的类时，未加包名

--- WXWeb/__init__.py
+++ WXWeb/__init__.py
@@ -6,8 +6,8 @@ import basic
 import menu
 
 
-wxMenu = Menu()
-wxBasic = Basic()
+wxMenu = menu.Menu()
+wxBasic = basic.Basic()
 accessToken = wxBasic.get_access_token()
 #myMenu.delete(accessToken)
 wxMenu.create(wxBasic.menuJson, accessToken)
