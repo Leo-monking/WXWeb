994391c7da47838ce143ca23c326c9470af3aa18
Leo-monking <m_hmj@126.com>
2017-08-21 14:42:16 +0800

    access_token
    access_token添加测试代码

--- utils/basic.py
+++ utils/basic.py
@@ -17,16 +17,16 @@ class Basic():
 		self.__accessToken = ''
 		self.__leftTime = 0
 	def __real_get_access_token(self,appId,appSecret):
-		print "__real_get_access_token(self,appId,appSecret):"
 		if (appId == "") or (appSecret ==""):
 			#appId = "wx5f490cedc2deb9e6"
 			#appSecret = "c5988955612e8303770788b4da00a997"
 			appId = "wxc42cdded0a80792a"
 			appSecret = "802c6edba56dfa685f05cadb2228230a"
 		postUrl = ("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s" % (appId, appSecret))
-		print postUrl
+		print "access_token get:",postUrl
 		urlResp = urllib.urlopen(postUrl)
-		urlResp = json.loads(urlResp.read())
+		rspRet = urlResp.read()
+		urlResp = json.loads(rspRet)
 		if "access_token" in urlResp.keys():
 			self.__accessToken = urlResp['access_token']
 			self.__leftTime = urlResp['expires_in']
@@ -40,7 +40,6 @@ class Basic():
 		return self.__accessToken
 		
 	def run(self):
-		print "run(self):"
 		while(True):
 			if self.__leftTime > 10:
 				time.sleep(2)
