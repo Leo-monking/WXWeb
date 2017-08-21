40e7c2893bc2c30837a560e255dd69deac2babe3
Leo-monking <m_hmj@126.com>
2017-08-21 14:32:41 +0800

    调整菜单创建逻辑
    调整菜单创建逻辑：获取access_token成功后再创建菜单

--- WXWeb/__init__.py
+++ WXWeb/__init__.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8-*-
 import sys
 sys.path.append('utils')
-import urllib
 import basic
 import menu
 
 
-wxMenu = menu.Menu()
+
 wxBasic = basic.Basic()
 accessToken = wxBasic.get_access_token("","")
-#myMenu.delete(accessToken)
-wxMenu.create(menu.menuJson, accessToken)
+if accessToken != "":
+	wxMenu = menu.Menu()
+	#myMenu.delete(accessToken)
+	wxMenu.create(menu.menuJson, accessToken)
